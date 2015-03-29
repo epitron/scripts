70ce01a392d3507dcdc7a47331cc3d0484253c67
Chris Gahan <chris@ill-logic.com>
2015-03-29 03:03:33 -0400

    '.railsrc' and a nice custom rails template

---
+++ rc/rails_template.rb
@@ -0,0 +1,38 @@
+gem 'coffee-rails-source-maps'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'pg'
+gem 'simple_form'
+
+gem_group :development do
+  gem 'pry-rails'
+  gem 'pry-theme'
+  gem 'best_errors'
+  gem 'quiet_assets'
+  gem 'letter_opener'
+  gem 'faker'
+  gem 'did_you_mean'
+end
+
+environment 'config.action_mailer.delivery_method = :letter_opener', env: 'development'
+environment 'config.assets.js_compressor = :closure', env: 'assets'
+
+rakefile("db.rake") do
+  %{
+    namespace :db do
+      desc "Reimport everything"
+      task remigrate: [:drop, :create, :migrate]
+    end
+  }
+end
+
+generate "haml:application_layout", "convert"
+# rake "haml:erb2haml"
+
+after_bundle do
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'First commit.' }
+end
+
+run "subl ."
