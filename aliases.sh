1d6193649bc6fb3e848aef757fe699a10ffad7b2
epitron <chris@ill-logic.com>
2017-05-29 02:41:37 -0400

    'kill-bg-jobs' does what it says

--- aliases.sh
+++ aliases.sh
@@ -178,6 +178,7 @@ alias gs="git status"
 alias gd="git diff"
 alias ga="git add"
 alias gl="git log --graph --stat"
+alias glu="gl -u"
 alias gch="git checkout"
 # alias g[[="git stash"
 # alias g]]="git stash pop"
@@ -212,6 +213,15 @@ aur() {
   fi
 }
 
+kill-bg-jobs() {
+  jobs=`jobs -ps`
+  if [ "$jobs" == "" ]; then
+    echo "Couldn't find any running background jobs"
+  else
+    echo "killing jobs: $jobs"
+    kill -9 $jobs
+  fi
+}
 alias gcs="gc --depth=1"
 
 # ruby
