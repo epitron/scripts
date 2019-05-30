91b511919d3b9ccc5af9d900489c95226271b370
epitron <chris@ill-logic.com>
2019-05-30 16:44:49 -0400

    'devicemapper-concatenate.sh' given a fuller example

--- snippets/devicemapper-concatenate.sh
+++ snippets/devicemapper-concatenate.sh
@@ -1,10 +1,17 @@
 # How to combine multiple files into one contiguous file with devicemapper
 
+# for testing, Create 4 files
+echo foo | dd of=block0 cbs=512 conv=block
+echo bar | dd of=block1 cbs=512 conv=block
+echo bat | dd of=block2 cbs=512 conv=block
+echo baz | dd of=block3 cbs=512 conv=block
+
 # Create a loop device for each of the 4 files
 losetup /dev/loop0 block0
 losetup /dev/loop1 block1
 losetup /dev/loop2 block2
 losetup /dev/loop3 block3
+
 # Create a device map named "test" using those loop devices
 (
     echo "0 1 linear /dev/loop0 0"
@@ -12,4 +19,5 @@ losetup /dev/loop3 block3
     echo "2 1 linear /dev/loop2 0"
     echo "3 1 linear /dev/loop3 0"
 ) | dmsetup create test
-$EDITOR /dev/mapper/test # use overwrite mode only
+
+cat /dev/mapper/test
