e807f964d333cab7cc58075e4cd59ccd00f1dfd6
epitron <chris@ill-logic.com>
2019-07-03 12:56:43 -0400

    firefox policies and security settings

---
+++ rc/firefox/user-customizations.js
@@ -0,0 +1,19 @@
+/* Find as you type
+  see http://website-archive.mozilla.org/www.mozilla.org/access/access/type-ahead/
+
+ Only enable for links. Type "'" then some text that match the link title!
+ */
+user_pref ("accessibility.typeaheadfind", false);
+user_pref("accessibility.typeaheadfind.autostart", true);
+user_pref("accessibility.typeaheadfind.casesensitive", 0);
+user_pref("accessibility.typeaheadfind.enablesound", false);
+user_pref("accessibility.typeaheadfind.enabletimeout", false);
+user_pref("accessibility.typeaheadfind.linksonly", true);
+user_pref("accessibility.typeaheadfind.startlinksonly", true);
+
+/* Search engine
+ let's use DuckDuckGo as the default!
+ */
+user_pref("browser.search.defaultenginename", "DuckDuckGo");
+user_pref("browser.search.countryCode", "CA");
+user_pref("browser.search.region", "CA");
